546b985243c1cd302a7770836d85f69d059d90ea
Harminder virk <virk.officials@gmail.com>
2020-07-05 10:24:41 +0530

    feat: add prettier

--- src/Hooks/index.ts
+++ src/Hooks/index.ts
@@ -5,7 +5,7 @@
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
-*/
+ */
 
 import { IocResolverContract, IocResolverLookupNode } from '@adonisjs/fold'
 
@@ -20,108 +20,107 @@ type HooksHandler = (...args: any[]) => void | Promise<void>
  * for user facing objects.
  */
 export class Hooks {
-  private hooks: {
-    before: Map<string, Set<HooksHandler | IocResolverLookupNode>>,
-    after: Map<string, Set<HooksHandler | IocResolverLookupNode>>,
-  } = {
-    before: new Map(),
-    after: new Map(),
-  }
+	private hooks: {
+		before: Map<string, Set<HooksHandler | IocResolverLookupNode>>
+		after: Map<string, Set<HooksHandler | IocResolverLookupNode>>
+	} = {
+		before: new Map(),
+		after: new Map(),
+	}
 
-  constructor (private resolver?: IocResolverContract) {
-  }
+	constructor(private resolver?: IocResolverContract) {}
 
-  /**
-   * Raise exceptins when resolver is not defined
-   */
-  private ensureResolver () {
-    if (!this.resolver) {
-      throw new Error('IoC container resolver is required to register string based hooks handlers')
-    }
-  }
+	/**
+	 * Raise exceptins when resolver is not defined
+	 */
+	private ensureResolver() {
+		if (!this.resolver) {
+			throw new Error('IoC container resolver is required to register string based hooks handlers')
+		}
+	}
 
-  /**
-   * Resolves the hook handler using the resolver when it is defined as string
-   * or returns the function reference back
-   */
-  private resolveHandler (handler: HooksHandler | string): HooksHandler | IocResolverLookupNode {
-    if (typeof (handler) === 'string') {
-      this.ensureResolver()
-      return this.resolver!.resolve(handler)
-    }
+	/**
+	 * Resolves the hook handler using the resolver when it is defined as string
+	 * or returns the function reference back
+	 */
+	private resolveHandler(handler: HooksHandler | string): HooksHandler | IocResolverLookupNode {
+		if (typeof handler === 'string') {
+			this.ensureResolver()
+			return this.resolver!.resolve(handler)
+		}
 
-    return handler
-  }
+		return handler
+	}
 
-  /**
-   * Returns a boolean whether a handler has been already registered or not
-   */
-  public has (lifecycle: 'before' | 'after', action: string, handler: HooksHandler | string): boolean {
-    const handlers = this.hooks[lifecycle].get(action)
-    const resolvedHandler = this.resolveHandler(handler)
-    if (!handlers) {
-      return false
-    }
+	/**
+	 * Returns a boolean whether a handler has been already registered or not
+	 */
+	public has(lifecycle: 'before' | 'after', action: string, handler: HooksHandler | string): boolean {
+		const handlers = this.hooks[lifecycle].get(action)
+		const resolvedHandler = this.resolveHandler(handler)
+		if (!handlers) {
+			return false
+		}
 
-    return handlers.has(resolvedHandler)
-  }
+		return handlers.has(resolvedHandler)
+	}
 
-  /**
-   * Register hook handler for a given event and lifecycle
-   */
-  public add (lifecycle: 'before' | 'after', action: string, handler: HooksHandler | string): this {
-    const handlers = this.hooks[lifecycle].get(action)
-    const resolvedHandler = this.resolveHandler(handler)
+	/**
+	 * Register hook handler for a given event and lifecycle
+	 */
+	public add(lifecycle: 'before' | 'after', action: string, handler: HooksHandler | string): this {
+		const handlers = this.hooks[lifecycle].get(action)
+		const resolvedHandler = this.resolveHandler(handler)
 
-    if (handlers) {
-      handlers.add(resolvedHandler)
-    } else {
-      this.hooks[lifecycle].set(action, new Set([resolvedHandler]))
-    }
+		if (handlers) {
+			handlers.add(resolvedHandler)
+		} else {
+			this.hooks[lifecycle].set(action, new Set([resolvedHandler]))
+		}
 
-    return this
-  }
+		return this
+	}
 
-  /**
-   * Remove a pre-registered handler
-   */
-  public remove (lifecycle: 'before' | 'after', action: string, handler: HooksHandler | string): void {
-    const handlers = this.hooks[lifecycle].get(action)
-    if (!handlers) {
-      return
-    }
+	/**
+	 * Remove a pre-registered handler
+	 */
+	public remove(lifecycle: 'before' | 'after', action: string, handler: HooksHandler | string): void {
+		const handlers = this.hooks[lifecycle].get(action)
+		if (!handlers) {
+			return
+		}
 
-    const resolvedHandler = this.resolveHandler(handler)
-    handlers.delete(resolvedHandler)
-  }
+		const resolvedHandler = this.resolveHandler(handler)
+		handlers.delete(resolvedHandler)
+	}
 
-  /**
-   * Remove a pre-registered handler
-   */
-  public clear (lifecycle: 'before' | 'after', action?: string): void {
-    if (!action) {
-      this.hooks[lifecycle].clear()
-      return
-    }
+	/**
+	 * Remove a pre-registered handler
+	 */
+	public clear(lifecycle: 'before' | 'after', action?: string): void {
+		if (!action) {
+			this.hooks[lifecycle].clear()
+			return
+		}
 
-    this.hooks[lifecycle].delete(action)
-  }
+		this.hooks[lifecycle].delete(action)
+	}
 
-  /**
-   * Executes the hook handler for a given action and lifecycle
-   */
-  public async exec (lifecycle: 'before' | 'after', action: string, ...data: any[]): Promise<void> {
-    const handlers = this.hooks[lifecycle].get(action)
-    if (!handlers) {
-      return
-    }
+	/**
+	 * Executes the hook handler for a given action and lifecycle
+	 */
+	public async exec(lifecycle: 'before' | 'after', action: string, ...data: any[]): Promise<void> {
+		const handlers = this.hooks[lifecycle].get(action)
+		if (!handlers) {
+			return
+		}
 
-    for (let handler of handlers) {
-      if (typeof (handler) === 'function') {
-        await handler(...data)
-      } else {
-        await this.resolver!.call(handler, undefined, data)
-      }
-    }
-  }
+		for (let handler of handlers) {
+			if (typeof handler === 'function') {
+				await handler(...data)
+			} else {
+				await this.resolver!.call(handler, undefined, data)
+			}
+		}
+	}
 }
